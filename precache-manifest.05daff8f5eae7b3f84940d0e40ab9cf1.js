f3d74d7e5bffd8a85743947d5e243df384908fc3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-13 02:20:56 +0000

    Deploy melsbelly/eats to github.com/melsbelly/eats.git:gh-pages

---
+++ precache-manifest.05daff8f5eae7b3f84940d0e40ab9cf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1745f7f70d52e687edb99ddf5dec12c",
+    "url": "/eats/index.html"
+  },
+  {
+    "revision": "9f33538af8d448912ca6",
+    "url": "/eats/static/css/main.50e0d94b.chunk.css"
+  },
+  {
+    "revision": "d4d1cb7a9ed3284393cb",
+    "url": "/eats/static/js/2.e91c9188.chunk.js"
+  },
+  {
+    "revision": "9f33538af8d448912ca6",
+    "url": "/eats/static/js/main.2a6c949f.chunk.js"
+  },
+  {
+    "revision": "17ace7ec26289b4be4ee",
+    "url": "/eats/static/js/runtime~main.839bf5cb.js"
+  }
+]);
